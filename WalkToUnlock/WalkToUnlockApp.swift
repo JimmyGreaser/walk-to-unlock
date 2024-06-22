be8e2a715aa83ff4e053e9061be7c1cecec0ac0f
Jimmy Greaser <jamesmichaelgreaser@gmail.com>
2024-06-22 15:18:00 +0100

    Initial Commit

---
+++ WalkToUnlock/WalkToUnlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalkToUnlockApp.swift
+//  WalkToUnlock
+//
+//  Created by Jimmy Greaser on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct WalkToUnlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
